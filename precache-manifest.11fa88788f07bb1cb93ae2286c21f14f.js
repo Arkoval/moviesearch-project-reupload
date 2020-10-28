0219d0c0ecf1175d1222c068a141b628d795c686
Arkoval <arkkowalski95@gmail.com>
2020-10-28 12:58:20 +0100

    Updates

---
+++ precache-manifest.11fa88788f07bb1cb93ae2286c21f14f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1bc8384c6c0b798b99c6f3a2b2da14",
+    "url": "/moviesearch-project-reupload/index.html"
+  },
+  {
+    "revision": "c2f5d5fba3c446be49bf",
+    "url": "/moviesearch-project-reupload/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "05a8eace4b90e1cb466e",
+    "url": "/moviesearch-project-reupload/static/css/main.92afc15f.chunk.css"
+  },
+  {
+    "revision": "c2f5d5fba3c446be49bf",
+    "url": "/moviesearch-project-reupload/static/js/2.83a03183.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearch-project-reupload/static/js/2.83a03183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a8eace4b90e1cb466e",
+    "url": "/moviesearch-project-reupload/static/js/main.1ce1c21d.chunk.js"
+  },
+  {
+    "revision": "e0e3bec0a9e9ba1abab7",
+    "url": "/moviesearch-project-reupload/static/js/runtime-main.63acd380.js"
+  },
+  {
+    "revision": "3ad635b0a840139235a08bc519403264",
+    "url": "/moviesearch-project-reupload/static/media/cinema-background.3ad635b0.jpg"
+  }
+]);
